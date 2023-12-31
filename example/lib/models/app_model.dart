63c330b1d7c3e6dd37fc3d3e7e7d6db86be526bb
juan <juandambra@balloon-group.com>
2023-12-30 21:32:00 -0300

    feat(flutter): :sparkles: Added username to model

--- example/lib/models/app_model.dart
+++ example/lib/models/app_model.dart
@@ -3,8 +3,15 @@ import 'package:flutter/foundation.dart';
 class AppModel extends ChangeNotifier {
   /// Internal, private state of the cart.
   bool _isDiscovering = false;
+  String _username = 'default username';
 
   bool get isDiscovering => _isDiscovering;
+  String get username => _username;
+
+  void changeUsername(String newUsername) {
+    _username = newUsername;
+    notifyListeners();
+  }
 
   void startDiscovery() {
     _isDiscovering = true;
