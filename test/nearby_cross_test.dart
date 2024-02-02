2e8dc834c09bbc20586bb994c25a13413fddeab1
Juan Pablo Donato <donatojp1996@gmail.com>
2024-02-01 23:22:14 -0300

    fix(flutter): :ambulance: Fix github test hook

--- test/nearby_cross_test.dart
+++ test/nearby_cross_test.dart
@@ -3,4 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 // TODO: Proper testing
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+
+  test('Default test', () {
+    expect(true, true);
+  });
 }
