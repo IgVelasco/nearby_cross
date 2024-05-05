d641810e0c45ea4b36b664d8053004840b153eb5
juan <juandambra@balloon-group.com>
2024-05-05 14:40:12 -0300

    fix pending tests

--- test/units/models/device_model_test.dart
+++ test/units/models/device_model_test.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:logger/logger.dart';
 import 'package:nearby_cross/models/device_model.dart';
 import 'package:nearby_cross/models/message_model.dart';
 import 'package:mockito/mockito.dart';
@@ -8,7 +12,7 @@ class MockNearby extends Mock implements NearbyCross {}
 
 void main() {
   test('Check messages on device', () {
-    var device = Device.fromConnection(MockNearby(), "123", "abc");
+    var device = Device.fromConnection(MockNearby(), "123", utf8.encode('abc'));
     device.addMessage(NearbyMessage.fromString("abc"));
     expect(device.getAllMessages().first.toString(), "abc");
   });
