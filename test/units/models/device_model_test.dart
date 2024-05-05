4f656fc7f64630b047f400e3f838ff1f9747e1a5
Juan Pablo Donato <donatojp1996@gmail.com>
2024-05-05 15:18:53 -0300

    Fix lint problems

--- test/units/models/device_model_test.dart
+++ test/units/models/device_model_test.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
-import 'dart:typed_data';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:logger/logger.dart';
 import 'package:nearby_cross/models/device_model.dart';
 import 'package:nearby_cross/models/message_model.dart';
 import 'package:mockito/mockito.dart';
